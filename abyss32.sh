29a7fe1aa7e32aedec77d3311ed965387c9cc6a2
MikaelBurton <majb_93@hotmail.com>
2018-03-11 18:11:01 -0600

    Update abyss32.sh

--- abyss32.sh
+++ abyss32.sh
@@ -1,5 +1,5 @@
-abyss-pe name=azotobacter32 k=32 in='SRR4149288_1.fastq SRR4149288_2.fastq' 
+abyss-pe name=azotobacter32 k=32 in='SRR4149288_1.fastq SRR4149288_2.fastq' # This is the command line to run the file and run the assembly  in abyss with the option of kmer=32 and -pe option for pairend libraries
 
-prokka azotobacter32-contigs.fa
+prokka azotobacter32-contigs.fa #This is the command line to run prokka with the file "-contigs.fa" generated with abyss, prokka made several files of the annotation
 
 
