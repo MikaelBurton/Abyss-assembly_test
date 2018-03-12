1d653b782d7c12c20dc72715a149b211a6ea96f2
MikaelBurton <majb_93@hotmail.com>
2018-03-11 18:11:31 -0600

    Update abyss64.sh

--- abyss64.sh
+++ abyss64.sh
@@ -1,5 +1,5 @@
-abyss-pe name=azotobacter64 k=64 in='SRR4149288_1.fastq SRR4149288_2.fastq' 
+abyss-pe name=azotobacter64 k=64 in='SRR4149288_1.fastq SRR4149288_2.fastq' # This is the command line to run the file and run the assembly  in abyss with the option of kmer=64 and -pe option for pairend libraries
 
-prokka azotobacter64-contigs.fa
+prokka azotobacter64-contigs.fa #This is the command line to run prokka with the file "-contigs.fa" generated with abyss, prokka made several files of the annotation
 
 
